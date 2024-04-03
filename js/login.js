4fed29941a79dc0c409637a8d4b14f50706a2f1b
martinbongani <martinbongani11@gmail.com>
2024-04-03 21:15:20 +0300

    Editing Admin, Accounts, Director, footer, login and CSS

--- js/login.js
+++ js/login.js
@@ -11,21 +11,34 @@ document.addEventListener("DOMContentLoaded", function() {
     const users = [
       { username: "admin", password: "123", userType: "admin" },
       { username: "parent", password: "test123", userType: "parent" },
-      { username: "staff", password: "test123", userType: "staff" },
+      { username: "director", password: "test123", userType: "staff" },
+      { username: "accounts", password: "test123", userType: "staff" },
+      { username: "procurement", password: "test123", userType: "staff" },
+      { username: "sitter", password: "test123", userType: "staff" },
+
     ];
 
     const user = users.find(user => user.username === username && user.password === password);
     if (user) {
-      switch (user.userType) {
+      switch (user.username) {
         case "admin":
           window.location.href = "/pages/admin.html";
           break;
         case "parent":
           window.location.href = "/pages/baby.html";
           break;
-        case "staff":
+        case "director":
           window.location.href = "/pages/director.html";
           break;
+          case "accounts":
+            window.location.href = "/pages/accounts.html";
+            break;
+            case "procurement":
+              window.location.href = "/pages/procurement.html";
+              break;  
+          case "sitter":
+            window.location.href = "/pages/sitter.html";
+            break;  
         default:
           break;
       }
