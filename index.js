8de7348d787895497e75dd1dc0730cfd061094f2
martinbongani <martinbongani11@gmail.com>
2024-04-09 20:43:34 +0300

    mongodb, dotenv, pug and path installation

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+// Dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+require('dotenv').config();
+
+// Instantiations
 const app = express();
+
+// Configurations
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  mongoose.connection
+    .once('open', () => {
+      console.log('Mongoose connection open');
+    })
+    .on('error', err => {
+      console.error(`Connection error: ${err.message}`);
+   });
+  
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.get('/home', (req, res) => {
     res.send ('Welcome to Daystar Daycare Center!');
